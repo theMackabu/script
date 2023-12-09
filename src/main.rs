e939c6bb2f391e0e4fce2fb702b146b2407ed6d0
theMackabu <theMackabu@gmail.com>
2023-12-09 12:29:57 -0800

    support status codes

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use macros_rs::{crashln, ternary};
 use reqwest::blocking::Client;
 use std::{fs, path::PathBuf};
 
-use rhai::{packages::Package, Engine, Map, ParseError, Scope, AST};
+use rhai::{packages::Package, plugin::*, Engine, FnNamespace, Map, ParseError, Scope, AST};
 use rhai_fs::FilesystemPackage;
 use rhai_url::UrlPackage;
 
-use actix_web::{get, http::header::ContentType, web::Path, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, http::header::ContentType, http::StatusCode, web::Path, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 // convert to peg
 lazy_static! {
@@ -33,34 +33,56 @@ fn convert_to_format(input: &str) -> String {
         .to_string()
 }
 
-fn text(string: String) -> (String, ContentType) { (string, ContentType::plaintext()) }
-fn html(string: String) -> (String, ContentType) { (string, ContentType::html()) }
-
-fn get(url: String) -> String {
-    let client = Client::new();
-    let response = match client.get(url).send() {
-        Ok(res) => res,
-        Err(err) => return err.to_string(),
-    };
-
-    if response.status().is_success() {
-        response.text().unwrap()
-    } else {
-        format!("request failed with status code: {}", response.status())
-    }
-}
-
-fn json(object: Map) -> (String, ContentType) {
-    match serde_json::to_string(&object) {
-        Ok(result) => (result, ContentType::json()),
-        Err(err) => (err.to_string(), ContentType::plaintext()),
-    }
+fn convert_status(code: i64) -> StatusCode {
+    let u16_code = code as u16;
+    StatusCode::from_u16(u16_code).unwrap_or(StatusCode::OK)
 }
 
 fn error(engine: &Engine, path: &str, err: ParseError) -> AST {
     match engine.compile(format!("fn {path}(){{text(\"error reading script file: {err}\")}}")) {
         Ok(ast) => ast,
         Err(_) => Default::default(),
+    }
+}
+
+#[export_module]
+mod default {
+    pub fn text(string: String) -> (String, ContentType, StatusCode) { (string, ContentType::plaintext(), StatusCode::OK) }
+    pub fn html(string: String) -> (String, ContentType, StatusCode) { (string, ContentType::html(), StatusCode::OK) }
+    pub fn json(object: Map) -> (String, ContentType, StatusCode) {
+        match serde_json::to_string(&object) {
+            Ok(result) => (result, ContentType::json(), StatusCode::OK),
+            Err(err) => (err.to_string(), ContentType::plaintext(), StatusCode::INTERNAL_SERVER_ERROR),
+        }
+    }
+}
+
+#[export_module]
+mod status {
+    pub fn text(string: String, status: i64) -> (String, ContentType, StatusCode) { (string, ContentType::plaintext(), convert_status(status)) }
+    pub fn html(string: String, status: i64) -> (String, ContentType, StatusCode) { (string, ContentType::html(), convert_status(status)) }
+    pub fn json(object: Map, status: i64) -> (String, ContentType, StatusCode) {
+        match serde_json::to_string(&object) {
+            Ok(result) => (result, ContentType::json(), convert_status(status)),
+            Err(err) => (err.to_string(), ContentType::plaintext(), StatusCode::INTERNAL_SERVER_ERROR),
+        }
+    }
+}
+
+#[export_module]
+mod http {
+    pub fn get(url: String) -> String {
+        let client = Client::new();
+        let response = match client.get(url).send() {
+            Ok(res) => res,
+            Err(err) => return err.to_string(),
+        };
+
+        if response.status().is_success() {
+            response.text().unwrap()
+        } else {
+            format!("request failed with status code: {}", response.status())
+        }
     }
 }
 
@@ -73,6 +95,7 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
     let filename: PathBuf = "app.routes".into();
     let fs_pkg = FilesystemPackage::new();
     let url_pkg = UrlPackage::new();
+    let http = exported_module!(http);
 
     let mut engine = Engine::new();
     let mut scope = Scope::new();
@@ -84,6 +107,7 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
 
     fs_pkg.register_into_engine(&mut engine);
     url_pkg.register_into_engine(&mut engine);
+    engine.register_static_module("http", http.into());
 
     scope
         .push_constant("path", url.to_string())
@@ -91,7 +115,13 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
         .push_constant("ver", format!("{:?}", req.version()))
         .push_constant("query", req.query_string().to_string());
 
-    engine.register_fn("get", get).register_fn("text", text).register_fn("json", json).register_fn("html", html);
+    engine
+        .register_fn("text", default::text)
+        .register_fn("json", default::json)
+        .register_fn("html", default::html)
+        .register_fn("text", status::text)
+        .register_fn("json", status::json)
+        .register_fn("html", status::html);
 
     let contents = match fs::read_to_string(&filename) {
         Ok(contents) => contents,
@@ -121,24 +151,25 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
 
     ast.set_source(filename.to_string_lossy().to_string());
 
-    let (body, content_type) = match engine.call_fn::<(String, ContentType)>(&mut scope, &ast, &path, ()) {
+    let (body, content_type, status_code) = match engine.call_fn::<(String, ContentType, StatusCode)>(&mut scope, &ast, &path, ()) {
         Ok(response) => response,
         Err(err) => {
             if has_wildcard || has_error_page {
                 engine
-                    .call_fn::<(String, ContentType)>(&mut scope, &ast, ternary!(has_wildcard, "_wildcard", "_error_404"), (err.to_string(),))
+                    .call_fn::<(String, ContentType, StatusCode)>(&mut scope, &ast, ternary!(has_wildcard, "_wildcard", "_error_404"), (err.to_string(),))
                     .unwrap()
             } else {
                 eprintln!("Error reading script file: {}\n{}", filename.to_string_lossy(), err);
                 (
                     format!("function not found.\ndid you create {url}()?\n\nyou can add * {{}} or 404 {{}} routes as well."),
                     ContentType::plaintext(),
+                    StatusCode::NOT_FOUND,
                 )
             }
         }
     };
 
-    HttpResponse::Ok().content_type(content_type).body(body)
+    HttpResponse::build(status_code).content_type(content_type).body(body)
 }
 
 #[actix_web::main]
