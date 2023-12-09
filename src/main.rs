03f7b7421c36dae623042ecebcb00bed1a97bfa4
theMackabu <theMackabu@gmail.com>
2023-12-09 01:31:28 -0800

    ast

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use macros_rs::{crashln, str, ternary};
-use once_cell::sync::Lazy;
-use regex::Regex;
-use std::{collections::BTreeMap, fs, path::PathBuf, sync::Mutex};
+use fancy_regex::{Captures, Error, Regex};
+use lazy_static::lazy_static;
+use macros_rs::{crashln, ternary};
+use reqwest::blocking::Client;
+use std::{fs, path::PathBuf};
 
 use rhai::{packages::Package, Engine, Map, ParseError, Scope, AST};
 use rhai_fs::FilesystemPackage;
@@ -9,9 +10,17 @@ use rhai_url::UrlPackage;
 
 use actix_web::{get, http::header::ContentType, web::Path, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-static GET_ROUTES: Lazy<Mutex<BTreeMap<String, String>>> = Lazy::new(|| Mutex::new(BTreeMap::new()));
+// convert to peg
+lazy_static! {
+    static ref R_INDEX: Result<Regex, Error> = Regex::new(r"index\s*\{");
+    static ref R_ERR: Result<Regex, Error> = Regex::new(r"(\b\d{3})\s*\{");
+    static ref R_DOT: Result<Regex, Error> = Regex::new(r"\.(\w+)\(\)\s*\{");
+    static ref R_FN: Result<Regex, Error> = Regex::new(r"(\w+)\((.*?)\)\s*\{");
+    static ref R_WILD: Result<Regex, Error> = Regex::new(r"\*\s*\{|wildcard\s*\{");
+    static ref R_SLASH: Result<Regex, Error> = Regex::new(r"(?<=[a-zA-Z])/(?=[a-zA-Z])");
+}
 
-fn remove_first(s: &str) -> &str {
+fn rm_first(s: &str) -> &str {
     let mut chars = s.chars();
     chars.next();
     chars.as_str()
@@ -20,17 +29,26 @@ fn remove_first(s: &str) -> &str {
 fn convert_to_format(input: &str) -> String {
     Regex::new(r"\.(\w+)")
         .unwrap()
-        .replace_all(&input.replace("/", "_"), |captures: &regex::Captures| format!("__d{}", remove_first(&captures[0])))
+        .replace_all(&input.replace("/", "_"), |captures: &Captures| format!("__d{}", rm_first(&captures[0])))
         .to_string()
-}
-
-fn get(path: String, fn_name: String) {
-    let mut routes = GET_ROUTES.lock().unwrap();
-    routes.insert(path, fn_name);
 }
 
 fn text(string: String) -> (String, ContentType) { (string, ContentType::plaintext()) }
 fn html(string: String) -> (String, ContentType) { (string, ContentType::html()) }
+
+fn get(url: String) -> String {
+    let client = Client::new();
+    let response = match client.get(url).send() {
+        Ok(res) => res,
+        Err(err) => return err.to_string(),
+    };
+
+    if response.status().is_success() {
+        response.text().unwrap()
+    } else {
+        format!("request failed with status code: {}", response.status())
+    }
+}
 
 fn json(object: Map) -> (String, ContentType) {
     match serde_json::to_string(&object) {
@@ -60,7 +78,7 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
     let mut scope = Scope::new();
 
     let path = match url.as_str() {
-        "" => "index".to_string(),
+        "" => "_index".to_string(),
         _ => convert_to_format(&url.clone()),
     };
 
@@ -80,20 +98,21 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
         Err(err) => crashln!("Error reading script file: {}\n{}", filename.to_string_lossy(), err),
     };
 
-    let has_error_page = Regex::new(r"\b\d{3}\s*\{").unwrap().is_match(&contents);
-    let has_wildcard = Regex::new(r"\*\s*\{|wildcard\s*\{").unwrap().is_match(&contents);
+    let has_error_page = R_ERR.as_ref().unwrap().is_match(&contents).unwrap();
+    let has_wildcard = R_WILD.as_ref().unwrap().is_match(&contents).unwrap();
 
+    // cache contents until file change
     let contents = {
-        let p_dot = Regex::new(r"\.(\w+)\(\)\s*\{").unwrap();
-        let p_fn = Regex::new(r"(\w+)\(\)\s*\{").unwrap();
-        let p_wild = Regex::new(r"\*\s*\{|wildcard\s*\{").unwrap();
+        let result = R_SLASH.as_ref().unwrap().replace_all(&contents, "_").to_string();
+        let result = R_INDEX.as_ref().unwrap().replace_all(&result, "_index() {").to_string();
+        let result = R_ERR.as_ref().unwrap().replace_all(&result, "_error_$1(err) {").to_string();
+        let result = R_DOT.as_ref().unwrap().replace_all(&result, |captures: &Captures| format!("__d{}", rm_first(&captures[0]))).to_string();
+        let result = R_FN.as_ref().unwrap().replace_all(&result, |captures: &Captures| format!("fn {}", &captures[0])).to_string();
 
-        let result = contents.replace("/", "_");
-        let result = p_dot.replace_all(&result, |captures: &regex::Captures| format!("__d{}", remove_first(&captures[0]))).to_string();
-        let result = p_fn.replace_all(str!(result), |captures: &regex::Captures| format!("fn {}", &captures[0])).to_string();
-
-        ternary!(has_wildcard, p_wild.replace_all(&result, "fn wildcard() {").to_string(), result)
+        ternary!(has_wildcard, R_WILD.as_ref().unwrap().replace_all(&result, "fn _wildcard(err) {").to_string(), result)
     };
+
+    println!("{contents}");
 
     let mut ast = match engine.compile(contents) {
         Ok(ast) => ast,
@@ -105,11 +124,16 @@ async fn handler(url: Path<String>, req: HttpRequest) -> impl Responder {
     let (body, content_type) = match engine.call_fn::<(String, ContentType)>(&mut scope, &ast, &path, ()) {
         Ok(response) => response,
         Err(err) => {
-            if has_wildcard {
-                engine.call_fn::<(String, ContentType)>(&mut scope, &ast, "wildcard", ()).unwrap()
+            if has_wildcard || has_error_page {
+                engine
+                    .call_fn::<(String, ContentType)>(&mut scope, &ast, ternary!(has_wildcard, "_wildcard", "_error_404"), (err.to_string(),))
+                    .unwrap()
             } else {
                 eprintln!("Error reading script file: {}\n{}", filename.to_string_lossy(), err);
-                (format!("function not found.\ndid you create {url}()?"), ContentType::plaintext())
+                (
+                    format!("function not found.\ndid you create {url}()?\n\nyou can add * {{}} or 404 {{}} routes as well."),
+                    ContentType::plaintext(),
+                )
             }
         }
     };
